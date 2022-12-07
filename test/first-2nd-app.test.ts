9fe7f18e25b1b0ff0cd575c46b53b821a08429f3
Prince Nsidibe <prince.nsidibe@globalaccelerex.com>
2022-12-07 18:53:35 +0100

    Initial commit

---
+++ test/first-2nd-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as First2NdApp from '../lib/first-2nd-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/first-2nd-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new First2NdApp.First2NdAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
