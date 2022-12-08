bb3b95387eae4184e4d44dfa1f4ab618692d3dad
Prince Nsidibe <prince.nsidibe@globalaccelerex.com>
2022-12-08 23:23:53 +0100

    First Commit

--- bin/first-2nd-app.ts
+++ bin/first-2nd-app.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { First2NdAppStack } from '../lib/first-2nd-app-stack';
+import { First2ndapp } from '../lib/first-2nd-app-stack';
 
 const app = new cdk.App();
-new First2NdAppStack(app, 'First2NdAppStack', {
+new First2ndapp(app, 'First2NdApp', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
